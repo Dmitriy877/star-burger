57fddb8b672bef0b645d010e4c4a688bda03813e
Dmitriy877 <dmitriybukin05@gmail.com>
2025-08-20 20:14:39 +0300

    Fix migration

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -147,7 +147,7 @@ class Order(models.Model):
 
     class Meta:
         verbose_name = 'Заказ'
-        verbose_name_plural = 'Заказы'
+        verbose_name_plural = 'Заказы' 
 
     def __str__(self):
         return f"{self.firstname} {self.phonenumber}"
