56cd4c40f55d8d8c4cdde05fde1e0f6b6e2a9dd4
Dmitriy877 <dmitriybukin05@gmail.com>
2025-09-03 16:07:39 +0300

    Add app locations and Location model to store locations coordinates
    Add app locations and Location model to store locations coordinates

---
+++ locations/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Location(models.Model):
+    address = models.CharField(
+        'название',
+        max_length=50,
+        null=False,
+        unique=True,
+        db_index=True,
+    )
+    lon = models.DecimalField('Долгота', max_digits=9, decimal_places=6)
+    lat = models.DecimalField('Широта', max_digits=9, decimal_places=6)
+    created_at = models.DateTimeField(
+        'Создано',
+        null=True,
+        db_index=True,
+        blank=True,
+        default=timezone.now()
+    )
+
+    class Meta:
+        verbose_name = 'Локация'
+        verbose_name_plural = 'Локации'
+
+    def __str__(self):
+        return self.address
--- restaurateur/views.py
+++ restaurateur/views.py
@@ -3,14 +3,13 @@ from django.shortcuts import redirect, render
 from django.views import View
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
-from django.db.models import Sum, F
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 
 import requests
-from environs import Env
 from foodcartapp.models import Product, Restaurant, Order, RestaurantMenuItem
+from locations.models import Location
 from geopy import distance
 import operator
 
@@ -98,21 +97,28 @@ def view_restaurants(request):
 def view_orders(request):
 
     def fetch_coordinates(apikey, address):
-        base_url = "https://geocode-maps.yandex.ru/1.x"
-        response = requests.get(base_url, params={
-            "geocode": address,
-            "apikey": apikey,
-            "format": "json",
-        })
-        response.raise_for_status()
-        found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+        try:
+            location = Location.objects.get(address=address)
+            return location.lat, location.lon
 
-        if not found_places:
-            return None
+        except Location.DoesNotExist:
 
-        most_relevant = found_places[0]
-        lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-        return lat, lon
+            base_url = "https://geocode-maps.yandex.ru/1.x"
+            response = requests.get(base_url, params={
+                "geocode": address,
+                "apikey": apikey,
+                "format": "json",
+            })
+            response.raise_for_status()
+            found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+
+            if not found_places:
+                return None
+            most_relevant = found_places[0]
+            lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+
+            location = Location.objects.create(address=address, lon=lon, lat=lat)
+            return location.lat, location.lon
 
     def get_distance_to_restaurant(restaurant_address, delivery_address, api_key):
         try:
